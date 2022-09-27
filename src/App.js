35f0b955c48c5b36e2293b930f88c63b284bbc88
Jiang <jiangyue0380@gmail.com>
2022-09-27 22:35:03 +1000

    completed CreatePost component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import NavBar from "./Components/NavBar/NavBar.jsx";
 import DisplayPost from "./Components/DisplayPosts/DisplayPosts.jsx";
-
+import CreatePost from "./Components/CreatePost/CreatePost.jsx";
 function App() {
 
-  const [post, setPost] = useState([
+  const [posts, setPosts] = useState([
     {name: 'David Lagrange', post:'I recently went into the worrs to search for the oldest tree'},
-    {name: 'JJ Vega', post: 'Its April Fools Dya! Give this a dislike if you really like it.:)'}
+    {name: 'JJ Vega', post: 'Its April Fools Dya! Give this a dislike if you really like it.:)'},
   ])
+
+  function addNewPost(post) {
+    let temPost = [post, ...posts];
+    setPosts(temPost);
+
+  }
     
   return (
     <div>
       <NavBar />
-      <DisplayPost post={post}></DisplayPost>
+      <CreatePost addNewPost={addNewPost}/>
+      <DisplayPost post={posts}></DisplayPost>
 
     </div>
   );
---
+++ src/Components/CreatePost/CreatePost.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const CreatePost = (props) => {
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: post
+        }
+        props.addNewPost(newPost)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type="text" value={name} onChange={(event) => setName(event.target.value)}></input>
+
+            <label>Post</label>
+            <input type="text" value={post} onChange={(event) => setPost(event.target.value)}></input>
+
+            <button type='submit'>Create</button>
+        </form>
+    );
+}
+ 
+export default CreatePost;
