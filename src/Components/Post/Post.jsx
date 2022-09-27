6e949aafde9a7c8c86c962d29537a16585a1260e
Jiang <jiangyue0380@gmail.com>
2022-09-27 21:58:24 +1000

    compelted Post and DisplayPosts components

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Posts = (prop) => {
+    return (
+    <div>
+        <h2>{prop.post.name}</h2>
+        <p>{prop.post.post}</p>
+        <button>like</button>
+        <button>dislike</button>
+    </div>
+    );
+}
+ 
+export default Posts;
